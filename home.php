62c4db9e81e7e6404536f94bbe4efb672927443a
Jamuna <jamunaparajuli890@gmail.com>
2025-05-26 12:24:01 +0545

    Delete test2.html

--- home.php
+++ home.php
@@ -17,30 +17,3 @@ if(!isset($user_id)){
     header('location:login.php');
     
 }
-
-
-
-=======
- <?php
-
- // Include the database configuration file (for databaes connection)
-include 'config.php';
-
- // Start the session to access session variables
-session_start();
-
- // Get the logged-in user's ID from the session
-$user_id = $_SESSION['user_id'];
-
- // Check if the user is not logged in (no user_id in session)
-if(!isset($user_id)){
-
- // If not logged in, redirect the user to the login page
-    header('location:login.php');
-    
-}
-
-
-
->>>>>>> cabe474b531ea5b263a35efce7734f05e4907fd1
- ?>
